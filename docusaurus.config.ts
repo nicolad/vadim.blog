cc6a7ca609e67f645dff5be827613a3186cedf0c
vadim.nicolai-EXT <nicolai.vadim@gmail.com>
2024-08-15 08:59:28 +0300

    Small improvement.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,12 +3,12 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "My Site",
+  title: "Vadim's blog",
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://vadim.blog",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
