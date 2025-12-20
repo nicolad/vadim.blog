53897de27b67bd4ec942d6aecb93d3e7b26bfa5b
Vadim Nicolai <vadim.nicolai@vitrifi.net>
2025-12-20 13:29:19 +0200

    Small improvement.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -64,6 +64,9 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
+    colorMode: {
+      defaultMode: "dark",
+    },
     navbar: {
       title: "Vadim's blog",
       logo: {
